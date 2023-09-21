001352135d58dd9b913fa888044cfbe0f003283b
Crazy Urus <crazyurus@vip.qq.com>
2023-09-21 19:43:29 +0800

    feat: 完成初版

---
+++ app/src/main/java/cn/crazyurus/browser/launcher/MainActivity.kt
@@ -0,0 +1,20 @@
+package cn.crazyurus.browser.launcher
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(Intent.ACTION_MAIN)
+        val componentName = ComponentName("com.android.browser", "com.android.browser.BrowserActivity")
+
+        intent.component = componentName
+
+        startActivity(intent)
+        finish()
+    }
+}
